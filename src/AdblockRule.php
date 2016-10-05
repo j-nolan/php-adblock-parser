82f01b13b9f888f552829e816139cb49629692e9
Limon Monte <limon.monte@gmail.com>
2016-10-05 03:37:40 +0200

    Add AdblockRule class and simple test of toRegex() method

---
+++ src/AdblockRule.php
@@ -0,0 +1,45 @@
+<?php
+namespace Limonte;
+
+class AdblockRule
+{
+    private $rule;
+
+    public function __construct($rule)
+    {
+        $this->rule = $rule;
+    }
+
+    /**
+     * @return  string
+     */
+    public function toRegex()
+    {
+        // Check if the rule isn't already regexp
+        if ($this->startswith('/') && $this->endswith('/')) {
+            $rule = mb_substr($this->rule, 1, mb_strlen($this->rule) - 2);
+
+            if (empty($this->rule)) {
+                throw new Exception("Invalid rule " . $this->rule);
+            }
+
+            return $rule;
+        }
+    }
+
+    private function startsWith($needle)
+    {
+        $length = mb_strlen($needle);
+        return (mb_substr($this->rule, 0, $length) === $needle);
+    }
+
+    private function endsWith($needle)
+    {
+        $length = mb_strlen($needle);
+        if ($length == 0) {
+            return true;
+        }
+
+        return (mb_substr($this->rule, -$length) === $needle);
+    }
+}
--- tests/AdblockParserTest.php
+++ tests/AdblockParserTest.php
@@ -2,6 +2,7 @@
 namespace Limonte\Tests;
 
 use Limonte\AdblockParser;
+use Limonte\AdblockRule;
 
 class AdblockParserTest extends \PHPUnit_Framework_TestCase
 {
@@ -12,5 +13,11 @@ class AdblockParserTest extends \PHPUnit_Framework_TestCase
         ]);
 
         $this->assertTrue($adblockParser->shouldBlock('http://abs.proxistore.com'));
+    }
+
+    public function testToRegex()
+    {
+        $adblockParser = new AdblockRule('/slashes should be trimmed/');
+        $this->assertEquals('slashes should be trimmed', $adblockParser->toRegex());
     }
 }
